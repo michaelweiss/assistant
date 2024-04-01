1e589d58b233339412db5c68bb6b7a766031267a
michaelweiss <weiss@sce.carleton.ca>
2024-03-31 22:22:23 -0400

    chatbot with (unconstrained) history

--- chatbot.py
+++ chatbot.py
@@ -5,19 +5,25 @@ class Chatbot():
         """
         self.client = client
         self.model = model
+        self.reset()
+
+    def reset(self):
+        """
+        Reset the chatbot.
+        """
+        self.messages = []
+        self.messages.append({"role": "system", 
+            "content": "You are a helpful assistant. Carefully follow the user's instructions."})
 
     def handle_request(self, request):
         """
         Handle the request.
         """
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant. Carefully follow the user's instructions."},
-            {"role": "user", "content": request}
-        ]
+        self.messages.append({"role": "user", "content": request})
         response = self.client.chat.completions.create(
             model=self.model,
             temperature=0, # low to reduce randomness
-            messages=messages
+            messages=self.messages
         )
         response = response.choices[0].message.content
         return response
